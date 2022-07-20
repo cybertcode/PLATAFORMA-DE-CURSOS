ba19da27cf618a43f2fd93c32267741bba52af83
cybert22 <cybertcode@gmail.com>
2022-07-19 22:10:51 -0500

    Creamos nuestra propia archivo de rutas
    Co-Authored-By: MKEVYN <40509580+cybert22@users.noreply.github.com>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    // public const HOME = '/dashboard'; original
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -35,6 +36,12 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+            /*********************************************
+             * Agregamos nuestra propio archivo de rutas *
+             *********************************************/
+            Route::middleware('web', 'auth')
+                ->prefix('admin') //Para agregar el prefijo
+                ->group(base_path('routes/admin.php'));
         });
     }
 
