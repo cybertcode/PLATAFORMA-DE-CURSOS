83dd8aa6060aef936274156c97829e38361718d8
cybert22 <cybertcode@gmail.com>
2022-07-20 18:03:45 -0500

    Recuperando registros de cursos
    cursos
    
    Co-Authored-By: MKEVYN <40509580+cybert22@users.noreply.github.com>

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\admin\Course;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __invoke()
+    {
+        // return Course::find(9)->rating;
+        $courses = Course::where('status', '3')->latest('id')->get();
+        return $courses;
+        return view('welcome', compact('courses'));
+    }
+}
