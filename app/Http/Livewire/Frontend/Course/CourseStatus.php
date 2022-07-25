86684a784de22441351852ed1a39c8a5b9727601
cybert22 <cybertcode@gmail.com>
2022-07-25 15:10:35 -0500

    Recuperando información que se va mostrar
    Información del proceso de curso
    
    Co-Authored-By: MKEVYN <40509580+cybert22@users.noreply.github.com>

--- app/Http/Livewire/Frontend/Course/CourseStatus.php
+++ app/Http/Livewire/Frontend/Course/CourseStatus.php
@@ -8,9 +8,16 @@ use App\Models\admin\Course;
 class CourseStatus extends Component
 {
     public $course;
+    public $current;
     public function mount(Course $course)
     {
         $this->course = $course;
+        foreach ($course->lessons as  $lesson) {
+            if (!$lesson->completed) {
+                $this->current = $lesson;
+                break;
+            }
+        }
     }
     public function render()
     {
--- resources/views/livewire/frontend/course/course-status.blade.php
+++ resources/views/livewire/frontend/course/course-status.blade.php
@@ -1,3 +1,41 @@
-<div>
-    {{ $course->title }}
+<div class="mt-8">
+    <div class="container grid grid-cols-3 gap-8">
+        <div class="col-span-2">
+            {!! $current->iframe !!}
+            {{ $current->name }}
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h1>{{ $course->title }}</h1>
+                <div class="flex items-center ">
+                    <figure>
+                        <img src="{{ $course->teacher->profile_photo_url }}" alt="">
+                    </figure>
+                    <div class="ml-2">
+                        <p>{{ $course->teacher->name }}</p>
+                        <a class="text-blue-500" href="">{{ '@' . Str::slug($course->teacher->name, '') }}</a>
+                    </div>
+                </div>
+                <ul>
+                    @foreach ($course->sections as $section)
+                        <li>
+                            <a class="font-bold" href="">{{ $section->name }}</a>
+                            <ul>
+                                @foreach ($section->lessons as $lesson)
+                                    <li>
+                                        <a href="">{{ $lesson->id }}@if ($lesson->completed)
+                                                Completado
+                                            @else
+                                                en curso
+                                            @endif
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </div>
 </div>
