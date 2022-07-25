780e3b732c3da567fbb261f01931c4bc4f89ee98
cybert22 <cybertcode@gmail.com>
2022-07-25 17:48:19 -0500

    Marcar unidad como culminada
    los botones y checkbox
    
    Co-Authored-By: MKEVYN <40509580+cybert22@users.noreply.github.com>

--- app/Http/Livewire/Frontend/Course/CourseStatus.php
+++ app/Http/Livewire/Frontend/Course/CourseStatus.php
@@ -24,6 +24,10 @@ class CourseStatus extends Component
                 break;
             }
         }
+        // Para asignar a current la última lección al llegar al final
+        if (!$this->current) {
+            $this->current = $course->lessons->last();
+        }
     }
     public function render()
     {
@@ -32,18 +36,32 @@ class CourseStatus extends Component
     public function changeLesson(Lesson $lesson)
     {
         $this->current = $lesson;
-        $this->index = $this->course->lessons->pluck('id')->search($lesson->id);
-        if ($this->index == 0) {
-            $this->previous = null;
-        } else {
-            $this->previous = $this->course->lessons[$this->index - 1];
-        }
-        if ($this->index == $this->course->lessons->count() - 1) {
-            $this->next = null;
-        } else {
-            $this->next = $this->course->lessons[$this->index + 1];
-        }
+        // $this->index = $this->course->lessons->pluck('id')->search($lesson->id);
+        // if ($this->index == 0) {
+        //     $this->previous = null;
+        // } else {
+        //     $this->previous = $this->course->lessons[$this->index - 1];
+        // }
+        // if ($this->index == $this->course->lessons->count() - 1) {
+        //     $this->next = null;
+        // } else {
+        //     $this->next = $this->course->lessons[$this->index + 1];
+        // }
     }
+    // Para agregar o quitar el curso culminado
+    public function completed()
+    {
+        if ($this->current->completed) {
+            //Eliminamos el registros
+            $this->current->users()->detach(auth()->user()->id);
+        } else {
+            //agregamos registro
+            $this->current->users()->attach(auth()->user()->id);
+        }
+        $this->current = Lesson::find($this->current->id); //para marcar el check del curso completado
+        $this->course = Course::find($this->course->id); //para marcar el curso completado
+    }
+    // propiedades computadas
     public function getIndexProperty()
     {
         return $this->course->lessons->pluck('id')->search($this->current->id);
@@ -63,5 +81,17 @@ class CourseStatus extends Component
         } else {
             return $this->course->lessons[$this->index + 1];
         }
+    }
+    public function getAdvanceProperty()
+    {
+        $i = 0;
+        foreach ($this->course->lessons as $lesson) {
+            // es el atributo completed de abajo
+            if ($lesson->completed) {
+                $i++;
+            }
+        }
+        $advance = ($i * 100) / ($this->course->lessons->count());
+        return round($advance, 2);
     }
 }
--- resources/views/livewire/frontend/course/course-status.blade.php
+++ resources/views/livewire/frontend/course/course-status.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-8">
-    <div class="container grid grid-cols-3 gap-8">
-        <div class="col-span-2">
+    <div class="container grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div class="lg:col-span-2">
             <div class="embed-responsive">
                 {!! $current->iframe !!}
             </div>
@@ -13,8 +13,12 @@
                 </div>
             @endif
             <div>
-                <div class="flex items-center mt-4 cursor-pointer">
-                    <i class="fas fa-toggle-off text-2xl text-gray-600"></i>
+                <div class="flex items-center mt-4 cursor-pointer" wire:click="completed">
+                    @if ($current->completed)
+                        <i class="fas fa-toggle-on text-2xl text-blue-600"></i>
+                    @else
+                        <i class="fas fa-toggle-off text-2xl text-gray-600"></i>
+                    @endif
                     <p class="text-sm ml-2">Marcar como culminado</p>
                 </div>
             </div>
@@ -44,11 +48,11 @@
                             href="">{{ '@' . Str::slug($course->teacher->name, '') }}</a>
                     </div>
                 </div>
-                <p class="text-gray-600 text-sm mt-2">20% Completado</p>
+                <p class="text-gray-600 text-sm mt-2">{{ $this->advance . '%' }} completado</p>
                 <div class="relative pt-2">
                     <div class="overflow-hidden h-4 mb-4 text-xs flex rounded bg-gray-200">
-                        <div style="width:30%"
-                            class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500">
+                        <div style="width:{{ $this->advance . '%' }}"
+                            class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500 transition-all duration-500">
                         </div>
                     </div>
                 </div>
