c8d4e981f9684bac3a41b5d497883155d645287e
cybert22 <cybertcode@gmail.com>
2022-07-25 19:48:35 -0500

    Instalar laravel Permission
    instalamos y configuramos
    
    Co-Authored-By: MKEVYN <40509580+cybert22@users.noreply.github.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,9 +14,11 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    use HasRoles;
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
